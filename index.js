0ddeaceb5a5e48e0e5c8ef6078058cdcf2a8ba6e
Jon Kaplon <jon.kaplon1@gmail.com>
2021-12-12 18:12:39 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+let cards = []
+// let names = ['ben', 'jon', 'amy']
+
+const writeCards = function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        let newCard = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        cards.push(newCard)
+    }
+    return cards
+}
+
+
+const countDown = function countdown(int) {
+    for (let i = int; i >= 0; i--) {
+        console.log(i)
+    }
+}
